e868bfb4d4b61420e6bbb363d054873d199c569c
Michael Fisher <fisherdesign3@gmail.com>
2022-05-21 23:35:38 -0600

    All save buttons will run the save data function. Trying to figure out how to pull the correct text

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,19 @@
-//Variables
+//Global Variables
 var rowGen = document.getElementById('timeblocks');
 var times = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM',
 '3PM', '4PM', '5PM'];
 var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
 var timer = document.getElementById('currentDay');
-var hour = 18;
+var hour = moment().get('hour');
+
 //Page Load
 updater();
 chartSetup();
 colorCheck();
 console.log(hour);
+
 // Functions
+//This function will display and update the time under the header every second.
 function updater(){
   setInterval(function(){
     var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
@@ -18,55 +21,64 @@ function updater(){
     hour = moment().get('hour');
     timer.innerText = currentTime;
   },1000);} 
-
+// This function will generate and oragnize a div a text area and button for each time.
 function chartSetup (){
-  times.forEach (function(item, index, array){
+  //Using for each we can change what hours are included ata any time by updating the times array.
+  times.forEach (function(item, index, array){ 
+    //This section setups up all needed variables specific to this function.
       var divEl = document.createElement('div');
       var divTime = document.createElement('div');
       var divText = document.createElement('textarea');
       var divSave = document.createElement('button');
-
+    //This section creates a new row for each time.
       divEl.id = "rows" + index;
       divEl.classList.add("row");
-
+    // This sections creates the 1st column that shows the time.
       divTime.id = item + "slot";
-      divTime.innerText = item;
-      divTime.classList.add("col-md-1");
+      divTime.innerText = item; 
+      divTime.classList.add("col-md-1"); 
       divTime.classList.add("timeBlock");
-    
+    //This section creates the text area the user can type in.
       divText.id = item;
       divText.classList.add("col-md-10");
       divText.classList.add("textBlock");
-
+    // THis section creates the save button in the third column.
       divSave.id = item + "save";
       divSave.innerText = "Save";
       divSave.classList.add("btn")
       divSave.classList.add('btn-primary')
       divSave.classList.add("col-md-1");
       divSave.classList.add("saveBlock");
-
+    // After creating each element we post them to the page.
       rowGen.appendChild(divEl);
       timeBlock = document.getElementById('rows' + index)
       timeBlock.appendChild(divTime);
       timeBlock.appendChild(divText);
       timeBlock.appendChild(divSave);
+      // This makes it so each button can be clicked.
+      document.getElementById(item+"save").addEventListener("click",saveData);
   })
 }
+
 function colorCheck() {
+  // This function will update the color coordination of the page depending on what time is it.
   var color = document.getElementById("9AM");
   switch (hour) {
     case 9:
-      color = document.getElementById("9AM");
+      color = document.getElementById("9AM"); 
+      //Sets the current time to red.
       color.classList.add('present');
+      //Sets all times after the current to green.
       for (i = 1; i<8; i++){ 
         color = document.getElementById(times[i]);
         color.classList.add('future');
-      }
+      } 
     break;
     
     case 10:
-      color = document.getElementById('10AM');
+      color = document.getElementById('10AM'); 
       color.classList.add('present');
+      //Sets all times before current to gray.
       for (i = 0; i<1; i++){ 
         color = document.getElementById(times[i]);
         color.classList.add('past');
@@ -174,3 +186,11 @@ function colorCheck() {
     alert.appendChild(ooh);
   }
 }
+
+function saveData (event) {
+  event.preventDefault();
+  console.log(event);
+}
+
+
+
