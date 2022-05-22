b350fc7fbf428b3ab48234053a5649a73775a385
Michael Fisher <fisherdesign3@gmail.com>
2022-05-21 21:53:38 -0600

    Working on coloring in the correct times

--- Develop/script.js
+++ Develop/script.js
@@ -1,42 +1,52 @@
+//Variables
 var rowGen = document.getElementById('timeblocks');
 var times = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM',
 '3PM', '4PM', '5PM'];
 var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
 var timer = document.getElementById('currentDay');
-setInterval(function(){
+var hour = moment();
+
+
+//Scripts to Run:
+updater();
+console.log(hour);
+
+function updater(){
+  setInterval(function(){
     var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
     var timer = document.getElementById('currentDay');
+    hour = moment();
     timer.innerText = currentTime;
-  },1000);
+  },1000);} 
 
 times.forEach (function(item, index, array){
     var divEl = document.createElement('div');
-    var divEl1 = document.createElement('div');
-    var textEl = document.createElement('textarea');
-    var divEl3 = document.createElement('div');
+    var divTime = document.createElement('div');
+    var divText = document.createElement('textarea');
+    var divSave = document.createElement('button');
 
     divEl.id = "rows";
     divEl.classList.add("row");
 
-    divEl1.id = times[index];
-    divEl1.innerText = item;
-    divEl1.classList.add("col-md-1");
-    divEl1.classList.add("timeBlock");
-    
-
-    textEl.id = times[index] + "text";
-    textEl.classList.add("col-md-10");
-    textEl.classList.add("textBlock");
+    divTime.id = times[index];
+    divTime.innerText = item;
+    divTime.classList.add("col-md-1");
+    divTime.classList.add("timeBlock");
   
+    divText.id = times[index] + "text";
+    divText.classList.add("col-md-10");
+    divText.classList.add("textBlock");
 
-    divEl3.id = times[index] + "save";
-    divEl3.innerText = "Save";
-    divEl3.classList.add("col-md-1");
-    divEl3.classList.add("saveBlock");
-    
+    divSave.id = times[index] + "save";
+    divSave.innerText = "Save";
+    divSave.classList.add("btn")
+    divSave.classList.add('btn-primary')
+    divSave.classList.add("col-md-1");
+    divSave.classList.add("saveBlock");
+
     rowGen.appendChild(divEl);
     timeBlock = document.getElementById('rows')
-    timeBlock.appendChild(divEl1);
-    timeBlock.appendChild(textEl);
-    timeBlock.appendChild(divEl3);
+    timeBlock.appendChild(divTime);
+    timeBlock.appendChild(divText);
+    timeBlock.appendChild(divSave);
 })
